00b4b55989c555df304a1a2cf73d579c0a27ece1
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-15 18:13:15 -0600

    Implemented a word search function

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,29 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
-
+    ], 'SEINFELD');
     assert.isTrue(result);
   });
-});
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARR');
+    assert.isTrue(result);
+  });
+
+    it("should return false if the word is an empty array", function() {
+      const result = wordSearch([
+      ], 'HEY');
+
+      assert.isFalse(result);
+    });
+  });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  //creates an array of empty arrays everytime loop iteration
+  let newArray = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    newArray.push([]);
+  }
+  //nested loop to grab the row and col indexes
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      newArray[col][row] = matrix[row][col];
+      //reassign value of newArray to matrix 
     }
-}
+  }
+  return newArray;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (letters.length) {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+    const verticalJoin = transpose(letters).map(char => char.join(''));
+    for (letter of verticalJoin) {
+      if (letter.includes(word)) return true;
+    }
+  }
+  return false;
+};
+
+
+module.exports = wordSearch;
